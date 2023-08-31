cd62404e5cd3783db70d1613dec2f62d21801fee
Кондратенко Александр <helegven@yandex.ru>
2023-08-31 23:22:37 +1000

    update singal for admin notification

--- backend/api/signals.py
+++ backend/api/signals.py
@@ -11,13 +11,21 @@ def send_registration_email(sender, user, request, **kwargs):
             'Ваш аккаунт успешно зарегистрирован.'
             'Дождитесь активации вашего аккаунта Отделом Кадров'
         )
+        message_for_admin = (
+            f'Добавлен новый сотрудник - {user.email} '
+            'Присвойте ему токен для дальнейшей работы.'
+        )
         from_email = 'noreply@example.com'
         # указываем почтовый ящик от которого будет приходитьс рассылка
         recipient_list = [user.email]
+        stuff_recipient_list = ['admin@example.com']
         print(f"Sending email to: {recipient_list}")
 
         send_mail(subject, message, from_email, recipient_list)
         print("Email sent successfully")
+
+        send_mail(subject, message_for_admin, from_email, stuff_recipient_list)
+        print("Email-admin sent successfully")
 
     except Exception as e:
         print(f"Error sending email: {e}")
