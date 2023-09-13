bbc36ee54fce2b8f834c2768e416190c80453522
yuras_kv <u2@yuras-kvs-MacBook-Air.local>
2023-09-13 09:29:35 +0300

    изменил пермишены

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 
 class IsOrdinaryUser(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.method == 'POST' and view.action == 'send_for_review':
+        if request.method in permissions.SAFE_METHODS or 'POST' and view.action == 'send_for_review':
 
             return True
         return request.user.is_teamleader
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('', include('djoser.urls.authtoken')),
     path('', include(router.urls)),
     path('curent_user_info/', ShortUserProfileViewSet.as_view({'get': 'list'}), name='user_profile_info'),
-    path('users/my_notifications/', UserNotificationsViewSet.as_view({'get': 'list'}), name='user-notifications'),
+    path('user/my_notifications/', UserNotificationsViewSet.as_view({'get': 'list'}), name='user-notifications'),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('swagger/', SpectacularSwaggerView.as_view(url_name='api:schema'), name='swagger'),
 ]
