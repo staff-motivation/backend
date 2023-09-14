2aff6fa28440bd1763fb7369f173ffb94c7517cd
yuras_kv <u2@yuras-kvs-MacBook-Air.local>
2023-09-14 12:39:50 +0300

    исправил пару ошибок в начислении баллов

--- backend/management/commands/reset_monthly_reward_points.py
+++ backend/management/commands/reset_monthly_reward_points.py
@@ -5,10 +5,17 @@ from users.models import User
 
 
 class Command(BaseCommand):
-    help = 'Reset monthly reward points for all users'
+    help = 'Reset monthly reward points for all users and clear user achievements'
+
     def handle(self, *args, **kwargs):
         today = date.today()
         start_of_month = today.replace(day=1)
         end_of_month = today + relativedelta(day=31)
+
+        # Reset monthly reward points for all users
         User.objects.update(reward_points_for_current_month=0)
-        self.stdout.write(self.style.SUCCESS('Monthly reward points reset complete.'))
+
+        # Clear user achievements
+        User.objects.all().prefetch_related('achievements').update(achievements=None)
+
+        self.stdout.write(self.style.SUCCESS('Monthly reward points and user achievements reset complete.'))
