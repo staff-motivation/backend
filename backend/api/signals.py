0b421a8653ad01d3ee32d319b4f5c96a24d16891
Vadim Evlentev <vadim-evl@yandex.ru>
2023-09-26 05:23:13 +0300

    try email

--- backend/api/signals.py
+++ backend/api/signals.py
@@ -1,31 +1,28 @@
+from uuid import uuid4
+
+from django.conf import settings
 from django.core.mail import send_mail
 from django.dispatch import receiver
 from djoser.signals import user_registered
 
 
 @receiver(user_registered)
-def send_registration_email(sender, user, request, **kwargs):
-    try:
-        subject = 'Регистрация успешна'
-        message = (
-            'Ваш аккаунт успешно зарегистрирован.'
-            'Дождитесь активации вашего аккаунта Отделом Кадров'
-        )
-        message_for_admin = (
-            f'Добавлен новый сотрудник - {user.email} '
-            'Присвойте ему токен для дальнейшей работы.'
-        )
-        from_email = 'motivation-system@yandex.ru'
-        # указываем почтовый ящик от которого будет приходитьс рассылка
-        recipient_list = [user.email]
-        stuff_recipient_list = ['motivation-system@yandex.ru']
-        print(f'Sending email to: {recipient_list}')
+def send_email_with_confirmation_code(request):
+    subject = 'Comfirmartion code for Staff-motivation'
+    message = (
+        'Enter the confirmation code on the site to activate your account'
+    )
+    from_email = settings.DEFAULT_FROM_EMAIL
+    # указываем почтовый ящик от которого будет приходитьс рассылка
+    mail_to = request.data.get('email')
+    confirmation_code = uuid4().hex
+    print(f'Sending email to: {mail_to}')
 
-        send_mail(subject, message, from_email, recipient_list)
-        print('Email sent successfully')
-
-        send_mail(subject, message_for_admin, from_email, stuff_recipient_list)
-        print('Email-admin sent successfully')
-
-    except Exception as e:
-        print(f'Error sending email: {e}')
+    send_mail(
+        subject,
+        f'{message} = {confirmation_code}',
+        from_email,
+        [mail_to],
+        fail_silently=False,
+    )
+    print('Email sent successfully')
