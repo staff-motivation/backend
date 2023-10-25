4764e2cd158d2e8e341d33661db792fb507b2802
slavy <slav009@yandex.ru>
2023-10-25 14:37:46 +0300

    Added responses

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,6 +4,15 @@ from rest_framework import serializers
 from notifications.models import Notification
 
 
+class ProgressSerializer(serializers.Serializer):
+    personal_progress = serializers.IntegerField()
+    department_progress = serializers.IntegerField()
+
+
+class ErrorSerializer(serializers.Serializer):
+    detail = serializers.CharField()
+
+
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
--- backend/api/views.py
+++ backend/api/views.py
@@ -7,67 +7,70 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from api.serializers import NotificationSerializer
+from api.serializers import (
+    NotificationSerializer,
+    ProgressSerializer,
+    ErrorSerializer,
+)
 from notifications.models import Notification
 from tasks.models import Task
 
 
 class ProgressUserAndDepartmentAPI(GenericAPIView):
-    """API для получения прогресса пользователя и его департамента за месяц.
+    """
+    API для получения прогресса пользователя и его департамента за месяц.
     Значение в процентах от всех выполненных задач за месяц.
-
-    Пример ответа:
-
-    {
-        'personal_progress': user_percentage,
-        'department_progress': dep_percentage
-    }
-
     """
     permission_classes = [IsAuthenticated]
+    serializer_class = ProgressSerializer
 
+    @extend_schema(responses={
+        status.HTTP_200_OK: ProgressSerializer,
+        status.HTTP_401_UNAUTHORIZED: ErrorSerializer})
     def get(self, request):
         user = request.user
         today = date.today()
         start_of_month = today.replace(day=1)
         next_month = start_of_month + relativedelta(months=1)
 
-        department = user.department
-        if department is None:
-            return Response(
-                {'error': 'У пользователя отсутствует департамент.'},
-                status=status.HTTP_400_BAD_REQUEST,)
+        user_percentage = 0
+        dep_percentage = 0
+
         total_approved_tasks_count = Task.objects.filter(
             deadline__gte=start_of_month,
             deadline__lt=next_month,
             status=Task.APPROVED
         ).count()
-        if total_approved_tasks_count == 0:
-            return Response(
-                {'error': 'Нет выполненных задач в текущем месяце.'},
-                status=status.HTTP_400_BAD_REQUEST,)
-        user_approved_tasks_count = Task.objects.filter(
-            deadline__gte=start_of_month,
-            deadline__lt=next_month,
-            assigned_to=user.id,
-            status=Task.APPROVED
-        ).count()
-        dep_approved_tasks_count = Task.objects.filter(
-            deadline__gte=start_of_month,
-            deadline__lt=next_month,
-            status=Task.APPROVED,
-            department=department
-        ).count()
 
-        user_percentage = round(
-            100 * (user_approved_tasks_count / total_approved_tasks_count))
-        dep_percentage = round(
-            100 * (dep_approved_tasks_count / total_approved_tasks_count))
+        if total_approved_tasks_count > 0:
+            user_approved_tasks_count = Task.objects.filter(
+                deadline__gte=start_of_month,
+                deadline__lt=next_month,
+                assigned_to=user.id,
+                status=Task.APPROVED
+            ).count()
+            user_percentage = round(
+                100 * (user_approved_tasks_count / total_approved_tasks_count))
 
-        return Response({
+            department = user.department
+            if department is not None:
+                dep_approved_tasks_count = Task.objects.filter(
+                    deadline__gte=start_of_month,
+                    deadline__lt=next_month,
+                    status=Task.APPROVED,
+                    department=department
+                ).count()
+                dep_percentage = round(
+                    100 * (dep_approved_tasks_count /
+                           total_approved_tasks_count))
+
+        data = {
             'personal_progress': user_percentage,
             'department_progress': dep_percentage,
-        })
+        }
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid()
+        return Response(serializer.data)
 
 
 @extend_schema(description='Получение уведомлений пользователя.')
